11041a01000e42c2119793caca4d9931287cd0a6
Dawn Olivier <dmarieolivier@gmail.com>
2022-04-30 17:06:48 -0400

    commit tree 563c361b748801db3efb4a513744e93752bc3198

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer;
+const leastFavoriteCustomer = 'reassign me'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'attempting to reassign'
+}
